30c3952cba08d23d8e3876733bce75e88133fe79
foudil.chatra <foudil.chatra@epitech.eu>
2026-03-12 20:23:26 -0400

    [fix] chose selecte

--- launcher_python/config.py
+++ launcher_python/config.py
@@ -1,5 +1,5 @@
-DEFAULT_ENGINE = "atg-video-1"
+DEFAULT_ENGINE = "atg-video-2"
 
-ENGINE_PATH = "assets/engines/atg-video-1/05_honda_vtec.mr"
+ENGINE_PATH = "assets/engines/atg-video-2/05_odd_fire_v6.mr"
 
 ENGINE_SIM_EXEC = "./build/linux-release/engine-sim-app"
